b5c0ca0e24617f12592bb6a03f3cabf41f0e3862
Deepan <77338525+Deepan1904@users.noreply.github.com>
2024-02-18 23:00:16 +0530

    Update EmployeeService.js

--- employee-management/src/service/EmployeeService.js
+++ employee-management/src/service/EmployeeService.js
@@ -3,29 +3,32 @@ import axios from "axios";
 const URL = "http://localhost:8080/api/";
 
 // const RENDER_URL = "https://employee-management-service-d4ne.onrender.com/api/"
-const RENDER_URL = "https://employeemanagementservice-7gua.onrender.com/api/"
+//const RENDER_URL = "https://employeemanagementservice-7gua.onrender.com/api/";
+
+const koyeb_URL = "https://employee-management-o-ones.koyeb.app/api/";
+
 class EmployeeService {
     
     saveEmployee(employee)
     {
-        return axios.post(RENDER_URL+"save",employee);
+        return axios.post(koyeb_URL+"save",employee);
     }
 
     getAllEmployee(employee)
     {
-        return axios.get(RENDER_URL+"getAll",employee);
+        return axios.get(koyeb_URL+"getAll",employee);
     }
 
     deleteEmployeeByID(id){
-        return axios.delete(RENDER_URL+"deleteID/"+id);
+        return axios.delete(koyeb_URL+"deleteID/"+id);
     }
 
     getEmployeeByID(id){
-        return axios.get(RENDER_URL+"getbyId/"+id);
+        return axios.get(koyeb_URL+"getbyId/"+id);
     }
 
     updateEmployeeByID(employee){
-        return axios.put(RENDER_URL+"update",employee);
+        return axios.put(koyeb_URL+"update",employee);
     }
 }
 export default new EmployeeService();
